56062694865a1177ed972d46e60edecb69e1069d
Vasilis Danias <vasilios.danias@hec.edu>
2021-12-09 15:40:38 +0200

    Fixed hooks and added features

--- src/App.tsx
+++ src/App.tsx
@@ -1,170 +1,205 @@
-import React, { useState, useEffect, useRef, Dispatch } from 'react';
+import React, { useState, useEffect, MouseEventHandler } from 'react';
 import { FaTrash, FaPen } from 'react-icons/fa';
 import { v4 as uuid } from 'uuid';
-import { bitloops } from './bitloops';
+import { bitloops, todoWorkflow } from './bitloops';
 import './App.css';
 
 type Item = {
   text: string;
-  editable: boolean;
+  status: boolean;
   id: string;
 };
-const handleSubscribeUpdate = (data: any, getItems: any, setItems: Dispatch<React.SetStateAction<Item[]>>) => {
-  console.log('workflow.Update received', data);
-  // console.log('state length', items.length, itemsRef.current?.length);
-  console.log('state length', getItems().length);
-  // const findIndex = itemsRef.current?.findIndex((el) => el.id === data.updatedData.id);
-  const findIndex = getItems().findIndex((el: any) => el.id === data.updatedData.id);
-  if (findIndex !== undefined && findIndex !== -1) mutateItem(findIndex, { ...data.updatedData }, setItems);
+
+type BitloopsEventType = {
+  event: string;
+  bitloopsData: any;
+}
+
+const ViewStates = {
+  ALL: 'All',
+  ACTIVE: 'Active',
+  COMPLETED: 'Completed',
+}
+
+const Events = {
+  UPDATE: 'update',
+  DELETE: 'delete',
+  CREATE: 'create',
 };
 
-const mutateItem = (index: number, newProps: Record<string, any>, setItems: any) => {
-  console.log(`mutating data with ${index} new Props`, newProps);
-  setItems((prevItems: any[]) => {
-    const shallowCopiedItems = [...prevItems];
-    const item = { ...prevItems[index], ...newProps };
-    shallowCopiedItems[index] = item;
-    return shallowCopiedItems;
-  });
-};
+const getBitloopsEventInitialState = (): BitloopsEventType | undefined => undefined;
 
 function App() {
-  const [input, setInput] = useState('');
-  const [items, setItems] = useState<Item[]>([]);
-  const itemsRef = useRef<Item[]>();
-  itemsRef.current = items;
-
-  const todoWorkflow = async (payload: Record<string, unknown>) => {
-    try {
-      const response = await bitloops.request(
-        '88e761bf-4824-4974-96b4-45c7bf741f11',
-        '9eac4f54-d4db-4fe9-b5a4-f1c6d527c446',
-        payload
-      );
-      return [response, null];
-    } catch (error) {
-      console.error(error);
-      return [null, error];
-    }
-  };
+  const [editable, setEditable] = useState('');
+  const [data, setData] = useState([]);
+  const [newValue, setNewValue] = useState('');
+  const [bitloopsEvent, setBitloopsEvent] = useState(getBitloopsEventInitialState());
+  // const [view, setView] = useState(ViewStates.ALL);
 
   const fetchToDos = async () => {
     const [response, error] = await todoWorkflow({ command: 'getAll' });
     if (error) return;
     console.log('fetched All response', response);
-    setItems(response.data.map(({ text, id }: any) => ({ text, editable: false, id })));
+    setData(response.data);
   };
 
-  const handleSubscribeDelete = (data: any) => {
-    console.log('workflow.delete received', data);
-    const findIndex = itemsRef.current?.findIndex((el) => el.id === data.id);
-    console.log('received data.id', data.id, 'my items', items);
-    console.log('about to remove from subscription index', findIndex);
-    if (findIndex !== undefined && findIndex !== -1) removeLocalItem(findIndex);
+  const handleSubscribe = (payload: any, event: string) => {
+    console.log('received event', event, payload)
+    const bitloopsEvent = {
+      event,
+      bitloopsData: payload,
+    }
+    setBitloopsEvent(bitloopsEvent);
   };
 
-  const handleSubscribeCreate = (data: any) => {
-    console.log('workflow.create received', data.newData);
-    const findIndex = itemsRef.current?.findIndex((el) => el.id === data.newData.id);
-    if (findIndex !== -1) return;
-    setItems((prevData: any) => {
-      return [...prevData, { ...data.newData, editable: false }];
-    });
+  const addItem = async (e: any) => {
+    e.preventDefault();
+    const newItem = {
+      status: 'Active',
+      text: newValue,
+      id: uuid(),
+    };
+    await todoWorkflow({ command: Events.CREATE, key: newItem.id, data: newItem });
+    setNewValue('');
   };
 
-  const getItems = () => {
-    return items;
+  const removeItem = async (id: string) => {
+    await todoWorkflow({ command: Events.DELETE, key: id });
   };
 
-  const setUpSubscriptions = async () => {
-    await bitloops.subscribe('workflowEvents.ToDos.update', (data) => handleSubscribeUpdate(data, getItems, setItems));
-    await bitloops.subscribe('workflowEvents.ToDos.delete', handleSubscribeDelete);
-    await bitloops.subscribe('workflowEvents.ToDos.create', handleSubscribeCreate);
-  };
+  const editItem = async (e: any) => {
+    const id = e.target.id;
+    const value = e.target.value;
+    const newData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < newData.length; i += 1) {
+      if (newData[i].id === id) {
+        newData[i].text = value;
+        await todoWorkflow({ command: Events.UPDATE, key: id, data: newData[i] });
+        break;
+      }
+    }
+    setEditable('');
+  }
+
+  const updateLocalItem = (e: any) => {
+    const id = e.target.id;
+    const value = e.target.value;
+    const newData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < newData.length; i += 1) {
+      if (newData[i].id === id) {
+        newData[i].text = value;
+        setData(newData);
+        break;
+      }
+    }
+  }
+
+  const handleCheckbox = async (e: any) => {
+    const id = e.target.id;
+    const checked = e.target.checked;
+    const newData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < newData.length; i += 1) {
+      if (newData[i].id === id) {
+        newData[i].status = checked ? ViewStates.COMPLETED : ViewStates.ACTIVE;
+        await todoWorkflow({ command: Events.UPDATE, key: id, data: newData[i] });
+      }
+    }
+  }
 
   useEffect(() => {
+    async function subscribe() {
+      await bitloops.subscribe(`workflowEvents.ToDos.${Events.CREATE}`, (d) => handleSubscribe(d, Events.CREATE));
+      await bitloops.subscribe(`workflowEvents.ToDos.${Events.DELETE}`, (d) => handleSubscribe(d, Events.DELETE));
+      await bitloops.subscribe(`workflowEvents.ToDos.${Events.UPDATE}`, (d) => handleSubscribe(d, Events.UPDATE));
+    }
+    subscribe();
     fetchToDos();
-    setUpSubscriptions();
   }, []);
 
-  const addItem = async () => {
-    const newItem = { text: input, id: uuid() };
-    setItems((prevData: any) => {
-      return [...prevData, { ...newItem, editable: false }];
-    });
-    setInput('');
-    await todoWorkflow({ command: 'create', key: newItem.id, data: newItem });
-  };
+  useEffect(() => {
+    if (bitloopsEvent) {
+      console.log('event', bitloopsEvent);
+      const { bitloopsData, event } = bitloopsEvent;
+      const updatedArray = JSON.parse(JSON.stringify(data));
 
-  const removeLocalItem = (index: number) => {
-    setItems((prevItems: any) => {
-      const shallowCopiedItems = [...prevItems];
-      shallowCopiedItems.splice(index, 1);
-      return shallowCopiedItems;
-    });
-  };
-
-  const removeItem = async (index: number) => {
-    const id = items[index].id;
-    removeLocalItem(index);
-    await todoWorkflow({ command: 'delete', key: id });
-  };
-
-  const handleEdit = (event: any, index: number) => {
-    const { value } = event.target;
-    mutateItem(index, { text: value }, setItems);
-  };
-
-  const handleUpdate = async (event: any, index: number) => {
-    console.log('enter pressed!');
-    mutateItem(index, { editable: false }, setItems);
-    const { text } = items[index];
-    await todoWorkflow({ command: 'update', key: items[index].id, data: { text } });
-  };
+      switch (event) {
+        case Events.CREATE: 
+          updatedArray.push(bitloopsData.newData);
+          setData(updatedArray);
+          break;
+        case Events.DELETE:
+          for (let i = 0; i < updatedArray.length; i += 1) {
+            if (updatedArray[i].id === bitloopsData.id) {
+              updatedArray.splice(i, 1);
+              break;
+            }
+          }
+          setData(updatedArray);
+          break;
+        case Events.UPDATE:
+          for (let i = 0; i < updatedArray.length; i += 1) {
+            if (updatedArray[i].id === bitloopsData.updatedData.id) {
+              updatedArray[i] = bitloopsData.updatedData;
+              break;
+            }
+          }
+          setData(updatedArray);
+          break;
+        default:
+          break;
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [bitloopsEvent]);
 
   return (
     <div className="todolist">
       <div className="heading">
-        <h1 className="title">To-Do List</h1>
+        <h1 className="title">Bitloops To-Do Demo</h1>
       </div>
       <input
         type="text"
-        value={input}
-        onChange={(event) => {
-          setInput(event.target.value);
-        }}
+        value={newValue}
+        className='todolistinput'
+        onChange={(e) => { setNewValue(e.target.value)}}
       />
       <button onClick={addItem}>Add</button>
 
       <div className="items">
         <ul>
-          {items.map(({ text, editable }, index: any) => (
-            <li key={index}>
-              {editable ? (
+          {data.map(({ text, id, status }) => (
+            <li key={id}>
+              <input className='checkbox' id={id} type="checkbox" defaultChecked={status === ViewStates.COMPLETED} onClick={handleCheckbox} />
+              {editable === id ? (
                 <input
                   type="text"
                   value={text}
-                  onChange={(event) => handleEdit(event, index)}
-                  onKeyPress={(event) => event.key === 'Enter' && handleUpdate(event, index)}
-                  onBlur={(event) => handleUpdate(event, index)}
+                  id={id}
+                  className='todolistinput'
+                  onChange={updateLocalItem}
+                  onKeyPress={(event) => event.key === 'Enter' && editItem(event)}
+                  onBlur={editItem}
                 />
               ) : (
-                <p>{text}</p>
+                <p id={id} onClick={(e: React.MouseEvent<HTMLElement>) => { 
+                  const target = e.target as HTMLParagraphElement;
+                  setEditable(target.id); 
+                }}>{text}</p>
               )}
               <div className="actions">
-                <FaPen
+                {/* <FaPen
                   onClick={() => {
-                    setItems((prevItems: any[]) => {
-                      const shallowCopiedItems = [...prevItems];
-                      const item = { ...shallowCopiedItems[index], editable: true };
-                      shallowCopiedItems[index] = item;
-                      return shallowCopiedItems;
-                    });
+                    // setItems((prevItems: any[]) => {
+                    //   const shallowCopiedItems = [...prevItems];
+                    //   const item = { ...shallowCopiedItems[index], editable: true };
+                    //   shallowCopiedItems[index] = item;
+                    //   return shallowCopiedItems;
+                    // });
                   }}
-                />
+                /> */}
                 <FaTrash
                   onClick={() => {
-                    removeItem(index);
+                    removeItem(id);
                   }}
                 />
               </div>
--- src/bitloops.ts
+++ src/bitloops.ts
@@ -7,4 +7,18 @@ bitloops.authenticate({
   token: '',
 });
 
-export { bitloops };
+const todoWorkflow = async (payload: any) => {
+  try {
+    const response = await bitloops.request(
+      '88e761bf-4824-4974-96b4-45c7bf741f11',
+      '9eac4f54-d4db-4fe9-b5a4-f1c6d527c446',
+      payload
+    );
+    return [response, null];
+  } catch (error) {
+    console.error(error);
+    return [null, error];
+  }
+};
+
+export { bitloops, todoWorkflow };
